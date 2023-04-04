b29d5a480208bd570b255a8b2788651be4b90dd9
Zakella <zapolski.slava@gmail.com>
2023-04-04 16:48:21 +0300

    2.2-2.4

---
+++ src/main/java/com/example/tacocloud/model/Taco.java
@@ -0,0 +1,20 @@
+package com.example.tacocloud.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Taco {
+
+    @NotNull
+    @Size(min=5, message="Name must be at least 5 characters long")
+    private String name;
+
+
+    private List<Ingredient> ingredients;
+
+}
